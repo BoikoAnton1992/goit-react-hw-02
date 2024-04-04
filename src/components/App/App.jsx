b4a339c2f2df078e86d80189e31654371eeaf5ff
BoikoAnton1992 <145497164+BoikoAnton1992@users.noreply.github.com>
2024-04-04 15:07:40 +0300

    Add local storege

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Description from '../Description/Description';
 import Options from '../Options/Options';
 import Feedback from '../Feedback/Feedback';
 import Notification from '../Notification/Notification';
 
 export default function App() {
-  const [typesFeedback, setTypesFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [typesFeedback, setTypesFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem('feedbacks');
+    if (savedFeedback !== null) {
+      return JSON.parse(savedFeedback);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
+
+  useEffect(() => {
+    localStorage.setItem('feedbacks', JSON.stringify(typesFeedback));
+  }, [typesFeedback]);
 
   const { good, neutral, bad } = typesFeedback;
   const totalFeedback = good + neutral + bad;
